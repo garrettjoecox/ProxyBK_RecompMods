a0e962a31e9ab6675df191b19bb8d1add95dd71c
LittleCube <littlecubehax@gmail.com>
2025-02-27 17:55:33 -0500

    init bk mod repo

---
+++ src/always_tumble_jump.c
@@ -0,0 +1,23 @@
+#include "modding.h"
+#include "functions.h"
+#include "variables.h"
+
+RECOMP_IMPORT("*", int recomp_printf(const char* fmt, ...));
+
+// Patches a function in the base game that's used to check if the player should tumble in the air.
+RECOMP_PATCH bool player_isFallTumbling(void) {
+    recomp_printf("Tumbling\n");
+    /*s32 damage;
+
+    if (randf() < 0.005f) {
+        return TRUE;
+    }
+
+    if (player_isStable()) {
+        return FALSE;
+    }
+    if (!bafalldamage_get_damage(&damage)) {
+        return FALSE;
+    }*/
+    return TRUE;
+}
