c7b0f486272fa8a5699f9acd1c2ad85d7c2277b5
Garrett <garrettjcox@gmail.com>
2026-01-25 23:06:27 -0600

    ToggleTrot 0.0.1

---
+++ packages/ToggleTrot/README.md
@@ -0,0 +1,6 @@
+# ToggleTrot
+Makes Talon Trot toggleable instead of hold-to-run
+
+If you release Z within the first 10 frames of the toggle trot, you will not stop trotting 
+until you press and release Z again. If you hold Z for longer than 10 frames, releasing Z will
+stop the trot immediately like the original behavior.
---
+++ packages/ToggleTrot/src/toggle_trot.c
@@ -0,0 +1,41 @@
+#include "modding.h"
+#include "functions.h"
+#include "variables.h"
+#include "recomputils.h"
+#include "core2/statetimer.h"
+
+int bakey_released(s32 button_indx);
+int bakey_newlyReleased(s32 button_indx);
+
+int framesSinceTrotStarted = 0;
+bool holdingZ = FALSE;
+
+RECOMP_HOOK("bs_setState")
+void bs_setState(s32 state_id) {
+    if (state_id == 20) { // trot enter
+        framesSinceTrotStarted = 0;
+        holdingZ = FALSE;
+    }
+}
+
+RECOMP_PATCH int func_802A8C60(void) {
+    framesSinceTrotStarted++;
+
+    if (stateTimer_isActive(STATE_TIMER_3_TURBO_TALON)) {
+        return 0;
+    }
+
+    if (framesSinceTrotStarted < 10) {
+        return 0;
+    }
+
+    if (!holdingZ && bakey_held(BUTTON_Z)) {
+        holdingZ = TRUE;
+    }
+
+    if (holdingZ && !bakey_held(BUTTON_Z)) {
+        return 1;
+    }
+
+    return 0;
+}
